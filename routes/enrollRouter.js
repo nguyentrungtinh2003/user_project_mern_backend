b15bed8d7cc6702ff6d4332e7b7335a047018ba9
nguyentrungtinh <trungtinhn300@gmail.com>
2025-09-04 09:54:33 +0700

    refactor(enroll-router):change method get enroll

--- routes/enrollRouter.js
+++ routes/enrollRouter.js
@@ -3,6 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/add", enrollController.addEnroll);
-router.post("/get", enrollController.getEnroll);
+router.get("/get", enrollController.getEnroll);
 
 module.exports = router;
